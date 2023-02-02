c438b8c34afd201d8195ed68849c92a3b6ce2e13
Guillaume Lacroix <glacroix@c1r9s2.42madrid.com>
2023-02-02 15:16:24 +0100

    refactor

--- snake_to_camel.c
+++ snake_to_camel.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
 
-int main (int argc, char **argv)
+int isCAP(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
+int main(int argc, char **argv)
 {
 	int i = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
-			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-				argv[1][i++] += 32;
-			else
-				i++;
-		}
-		i = 0;
-		while (argv[1][i])
-		{
-			if (argv[1][i] == '_' && (argv[1][i+1] >= 'a' && argv[1][i] <= 'z'))
+			if (isCAP(argv[1][i]) > 0)
 			{
-				i++;
-				argv[1][i] -= 32;
+				write(1, "_", 1);
+				argv[1][i] += 32;
 				write(1, &argv[1][i], 1);
 				i++;
 			}
--- str_capitalize.c
+++ str_capitalize.c
@@ -1,61 +1,94 @@
 #include <unistd.h>
 
+int isalpha_low(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
 int main(int argc, char **argv)
 {
-	int j = 1;
 	int i = 0;
-	if (argc == 1)
-		return write(1, "\n", 1);
-	if (argc > 1)
+	int j = 1;
+	if (argc >= 2)
 	{
 		while (j < argc)
 		{
 			i = 0;
-			while (argv[j][i])
+			while (argv[j][i] != '\0')
 			{
-				if (argv[j][i] >= 'a' && argv[j][i] <= 'z')
+				if (argv[j][i] >= 65 && argv[j][i] <= 90)
 				{
-					argv[j][i] -= 32;
+					argv[j][i] += 32;
 					i++;
 				}
 				else
-				{
 					i++;
-				}
 			}
 			j++;
 		}
 		j = 1;
+		i = 0;
 		while (j < argc)
 		{
 			i = 0;
-			while (argv[j][i])
+			while(argv[j][i] != '\0')
 			{
-				if ((argv[j][i] >= 'A' && argv[j][i] <= 'Z') && (argv[j][i-1] == ' ' || argv[j][i-1] == '\0'))
+				if (isalpha_low(argv[j][0]) > 0)
 				{
-					write (1, &argv[j][i], 1);
+					argv[j][0] -= 32;
+					write(1, &argv[j][0], 1);
 					i++;
 				}
-				else if (argv[j][i] >= 'A' && argv[j][i] <= 'Z' && argv[j][i-1] != ' ')
+				else if (isalpha_low(argv[j][i]) > 0 && argv[j][i - 1] == ' ')
 				{
-					argv[j][i] += 32;
+					argv[j][i] -= 32;
 					write(1, &argv[j][i], 1);
-					i++;
-				}
-				else if (((argv[j][i] >= 0 && argv[j][i] <= 64) || (argv[j][i] >= 123 && argv[j][i] <= 127)))
-				{
-					write (1, &argv[j][i], 1);
 					i++;
 				}
 				else
 				{
 					write(1, &argv[j][i], 1);
 					i++;
-				}
+				}	
 			}
-			write (1, "\n", 1);
+			write(1, "\n", 1);
 			j++;
 		}
 	}
-	return 0;
+	if (argc < 2)
+		write(1, "\n", 1);
+	return (0);
 }
+
+/*
+Assignment name  : str_capitalizer
+Expected files   : str_capitalizer.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes one or several strings and, for each argument,
+capitalizes the first character of each word (If it's a letter, obviously),
+puts the rest in lowercase, and displays the result on the standard output,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string. If a word only has one letter, it must be
+capitalized.
+
+If there are no arguments, the progam must display \n.
+
+Example:
+
+$> ./str_capitalizer | cat -e
+$
+$> ./str_capitalizer "a FiRSt LiTTlE TESt" | cat -e
+A First Little Test$
+$> ./str_capitalizer "__SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+__second Test A Little Bit   Moar Complex$
+   But... This Is Not That Complex$
+     Okay, This Is The Last 1239809147801 But Not    The Least    T$
+$>
+
+*/
