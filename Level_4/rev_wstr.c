7ad0bdab729dce3d46705ce50be9094078196841
Guillaume Lacroix <glacroix@c1r8s1.42madrid.com>
2023-02-08 17:54:34 +0100

    unifinished rev_wstr

---
+++ Level_4/rev_wstr.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+
+/*TO BE CONTINUED*/
+
+int ft_isspace(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int start = 0;
+	int end = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i] != '\0')
+			i++;
+		i--;
+		end = i;
+		while (!ft_isspace(argv[1][i]))
+			i--;
+		i++;
+		start = i;
+		while (start <= end)
+			write(1, &argv[1][start++], 1);
+	}
+	write(1, "\n", 1);
+}
+
+/*
+Assignment name  : rev_wstr
+Expected files   : rev_wstr.c
+Allowed functions: write, malloc, free
+--------------------------------------------------------------------------------
+
+Write a program that takes a string as a parameter, and prints its words in 
+reverse order.
+
+A "word" is a part of the string bounded by spaces and/or tabs, or the 
+begin/end of the string.
+
+If the number of parameters is different from 1, the program will display 
+'\n'.
+
+In the parameters that are going to be tested, there won't be any "additional" 
+spaces (meaning that there won't be additionnal spaces at the beginning or at 
+the end of the string, and words will always be separated by exactly one space).
+
+Examples:
+
+$> ./rev_wstr "You hate people! But I love gatherings. Isn't it ironic?" | cat -e
+ironic? it Isn't gatherings. love I But people! hate You$
+$>./rev_wstr "abcdefghijklm"
+abcdefghijklm
+$> ./rev_wstr "Wingardium Leviosa" | cat -e
+Leviosa Wingardium$
+$> ./rev_wstr | cat -e
+$
+$>
+*/
